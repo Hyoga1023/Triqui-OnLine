3d89f9da472c72a2c5a7cef308f8f0f29bd6340f
Cesar Martinez <cesarmartinez99@gmail.com>
2024-12-28 14:53:50 -0500

    Actualiza juego

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb7bvHardr3x4Sqv9pSb8EYBWw7Tq0Q2Q",
+  authDomain: "juego-de-triqui.firebaseapp.com",
+  projectId: "juego-de-triqui",
+  storageBucket: "juego-de-triqui.firebasestorage.app",
+  messagingSenderId: "510617105718",
+  appId: "1:510617105718:web:fe099bbcaf556c8656af97"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
